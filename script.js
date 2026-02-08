7831c3565118657740b4963b4cfd8be1bf283f41
Pranav Raj <pranavraj17200803@gmail.com>
2026-02-08 09:19:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-const elements = [
-    { name: "Hydrogen", symbol: "H", number: 1, mass: "1.008" },
-    { name: "Helium", symbol: "He", number: 2, mass: "4.0026" },
-    { name: "Lithium", symbol: "Li", number: 3, mass: "6.94" },
-    { name: "Beryllium", symbol: "Be", number: 4, mass: "9.0122" },
-    { name: "Boron", symbol: "B", number: 5, mass: "10.81" }
-];
+fetch('https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json')
+  .then(res => res.json())
+  .then(data => {
+    const elements = data.elements;
+    const table = document.getElementById("table");
 
-const table = document.getElementById("table");
+    elements.forEach(el => {
+      const div = document.createElement("div");
+      div.className = "element";
+      div.innerHTML = `<b>${el.symbol}</b><br>${el.number}`;
 
-elements.forEach(el => {
-    const div = document.createElement("div");
-    div.className = "element";
-    div.innerHTML = `<b>${el.symbol}</b><br>${el.number}`;
-
-    div.addEventListener("mouseover", () => {
+      div.addEventListener("mouseover", () => {
         document.getElementById("name").textContent = el.name;
         document.getElementById("symbol").textContent = el.symbol;
         document.getElementById("number").textContent = el.number;
-        document.getElementById("mass").textContent = el.mass;
-    });
+        document.getElementById("mass").textContent = el.atomic_mass;
+      });
 
-    table.appendChild(div);
-});
+      table.appendChild(div);
+    });
+  });
